c5ff99e75f223da1b3b9662e8330055a058aec20
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-24 17:40:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+song="";
+function preload() {
+   song=loadSound("Harry Potter Theme Song.mp3") ;
+}
 function setup() {
 canvas=createCanvas(350,350);
 canvas.center();
@@ -7,4 +11,13 @@ video.hide();
 }
 function draw() {
 image(video,0,0,350,350);
+}
+function start() {
+    song.play();
+}
+function pause() {
+    song.pause();
+}
+function stop() {
+    song.stop();
 }
