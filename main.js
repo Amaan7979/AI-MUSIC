8b04bafa0f62db0ec80b41b840a52221bc2913bc
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-26 17:37:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload() {
    song=loadSound("Harry Potter Theme Song.mp3") ;
 }
@@ -8,16 +12,36 @@ canvas.center();
 video=createCapture(VIDEO);
 video.size(350,350);
 video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",got_poses);
 }
 function draw() {
 image(video,0,0,350,350);
 }
 function start() {
     song.play();
+    song.setVolume(0.6);
+    song.rate(1.0);
 }
 function pause() {
     song.pause();
 }
 function stop() {
     song.stop();
+}
+function modelLoaded(){
+    console.log("Model is Loaded");
+}
+function got_poses(results){
+    if(results.length>0){
+        console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log(leftWristX);
+    console.log(leftWristY);
+    console.log(rightWristX);
+    console.log(rightWristY);
+    }
 }
